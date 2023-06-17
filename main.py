4dfc1624f86e42d38a3bbe26638795e927474969
Abdnahid <68383656+abdnahid@users.noreply.github.com>
2023-06-17 19:04:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, send_file
+from flask_cors import CORS
+from openpyxl import Workbook
 import os
+from test_report import InspectionExcel
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/")
+def home():
+    return "Hey there! Its working!"
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+@app.route("/demo-product-list")
+def demo_product_list():
+    return send_file(os.path.join("demo-product-list.xlsx"))
+
+
+@app.route("/<noteId>")
+def test(noteId):
+    InspectionExcel(noteId).generate_excel_files()
+    return send_file(os.path.join("newR.xlsx"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
